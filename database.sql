4140dbe61a35b5bfcaf911d4e9596509a48e5fd0
Junior Fonseca <94936578+Juniorffonseca@users.noreply.github.com>
2023-01-28 15:25:14 -0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,4 @@
+LOAD DATA INFILE 'C:/Users/anonb/Downloads/drive-download-20230128T162805Z-001/medicos.csv'
+INTO TABLE teste.medicos
+FIELDS TERMINATED BY 'space';
+
---
+++ r1.R
@@ -0,0 +1,9 @@
+# Carregando pacotes
+
+# Carregando csvs
+medicos <- read.csv2('medicos.csv')
+membros <- read.csv2('membros.csv')
+prescricoes <- read.csv2('prescricoes.csv')
+visitados <- read.csv2('visitados.csv')
+
+
